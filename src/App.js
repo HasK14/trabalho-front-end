de7a0a3d5d841f71d2a70aaf8ad9bf0bd0a84aca
HasK14 <hask_lourenco@hotmail.com>
2023-06-26 23:56:29 -0300

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import ListaAnimes from "./pages/ListaAnimes";
 
 function App() {
-  return (
-    <div>
-      <ListaAnimes />
-    </div>
-  );
+  const routes = createBrowserRouter([
+    {
+      path: "/",
+      element: <ListaAnimes />,
+    },
+    {
+      path: "/buscar",
+      element: <Home />,
+    },
+  ]);
+
+  return <RouterProvider router={routes} />;
 }
 
 export default App;
